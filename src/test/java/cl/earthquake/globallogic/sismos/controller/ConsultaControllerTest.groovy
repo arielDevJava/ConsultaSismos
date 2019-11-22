5f922a1ac065fa9ac14a0440a2b85c5ec1889596
Ariel Acuña <abacuna@emergya.com>
2019-11-22 13:12:50 -0300

    se crea carcasa para test en spock y se diseña diagrama de secuencia en png

---
+++ src/test/java/cl/earthquake/globallogic/sismos/controller/ConsultaControllerTest.groovy
@@ -0,0 +1,20 @@
+package cl.earthquake.globallogic.sismos.controller
+
+import spock.lang.Specification
+
+class ConsultaControllerTest extends Specification {
+    def "GetSismosByFechas"() {
+    }
+
+    def "GetSismosByMagnitudes"() {
+    }
+
+    def "GetSismosByRango"() {
+    }
+
+    def "GetSismosByPais"() {
+    }
+
+    def "GetSismosByPaisFecha"() {
+    }
+}
---
+++ src/test/java/cl/earthquake/globallogic/sismos/service/impl/ConsultaServiceImplTest.groovy
@@ -0,0 +1,20 @@
+package cl.earthquake.globallogic.sismos.service.impl
+
+import spock.lang.Specification
+
+class ConsultaServiceImplTest extends Specification {
+    def "GetSismosbyFecha"() {
+    }
+
+    def "GetSismosbyMagnitud"() {
+    }
+
+    def "GetSismosByRangoFecha"() {
+    }
+
+    def "GetSismosByPais"() {
+    }
+
+    def "GetSismosByPaisFecha"() {
+    }
+}
